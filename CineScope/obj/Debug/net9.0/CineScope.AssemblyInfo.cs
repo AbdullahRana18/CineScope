6aa024b7a1d1102be29151d2859d8df7ca510e40
Abdullah Rana <the.abdullah.1829@gmail.com>
2025-10-26 19:00:29 +0500

    fix Register UI

--- CineScope/obj/Debug/net9.0/CineScope.AssemblyInfo.cs
+++ CineScope/obj/Debug/net9.0/CineScope.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CineScope")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff119b19510f4b63fe94038aab4f72d3bdd1839b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4aed9c308492442e26d4f555623351dfcaaf2ac7")]
 [assembly: System.Reflection.AssemblyProductAttribute("CineScope")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CineScope")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
